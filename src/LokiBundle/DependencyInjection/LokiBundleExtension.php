ea7cc5ecd8adb9050534ce6c602eec647768cd9f
Jochen Schultz <jschultz@php.net>
2025-01-18 13:56:11 +0100

    fix namespaces

--- src/LokiBundle/DependencyInjection/LokiBundleExtension.php
+++ src/LokiBundle/DependencyInjection/LokiBundleExtension.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace IntelligentIntern\LokiBundle\DependencyInjection;
+namespace DefaultLogBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Config\FileLocator;
 
-class LokiBundleExtension extends Extension
+class DefaultLogBundleExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
