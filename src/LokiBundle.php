3ff25f65cfc32e6104cfc88c6f31986d35a2da2f
Jochen Schultz <jschultz@php.net>
2025-01-19 15:30:30 +0100

    Update namespaces and docblocks for v1.0.0

--- src/LokiBundle.php
+++ src/LokiBundle.php
@@ -4,13 +4,13 @@ namespace IntelligentIntern\LokiBundle;
 
 use IntelligentIntern\LokiBundle\DependencyInjection\Compiler\LogServiceCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class LokiBundle extends AbstractBundle
 {
-    public function build(ContainerBuilder $container): void
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
-        parent::build($container);
-        $container->addCompilerPass(new LogServiceCompilerPass());
+        $container->import(__DIR__ . '/../config/services.yaml');
     }
 }
