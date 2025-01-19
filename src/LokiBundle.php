b1a02a579a86bad3c41a542bf088d11e99cf742c
Jochen Schultz <jschultz@php.net>
2025-01-19 14:16:50 +0100

    Update namespaces and docblocks for v1.0.0

--- src/LokiBundle.php
+++ src/LokiBundle.php
@@ -4,9 +4,9 @@ namespace IntelligentIntern\LokiBundle;
 
 use IntelligentIntern\LokiBundle\DependencyInjection\Compiler\LogServiceCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class LokiBundle extends Bundle
+class LokiBundle extends AbstractBundle
 {
     public function build(ContainerBuilder $container): void
     {
